b2f4055a877f7137ba9969cab2b33e910d1fb8a1
Kanat <kanat_007xs@icloud.com>
2026-02-05 21:50:37 +0500

    bool

---
+++ Practice02/boolean/boolean_comparison.py
@@ -0,0 +1,17 @@
+#1
+print(100>5)
+#2
+a=input()
+b=input()
+if len(a)>len(b):
+    print(a)
+else:
+    print(b)
+#3
+a=int(input())
+if a>16 and a<23:
+    print("we can walk")
+elif a>0 and a<10:
+    print("we can go cino")
+else:
+    print("we dont go any where")
