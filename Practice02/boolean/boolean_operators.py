27f87b4c8db788fe68321634d6a14e5bb32bc139
Kanat <kanat_007xs@icloud.com>
2026-02-05 22:00:12 +0500

    bool

---
+++ Practice02/boolean/boolean_operators.py
@@ -0,0 +1,18 @@
+#1
+a=int(input())
+if a>18 and a<28:
+    print("you molodoy")
+elif a<18 and a>=1:
+    print("you baby")
+else:
+    print("you stary")
+#2
+men_red_flag=False
+if not men_red_flag:
+    print("we marry")
+#3
+a=int(input())
+if a>150 or a<200:
+    print("you can ")
+elif a<150 or a>200:
+    print("you cant")
