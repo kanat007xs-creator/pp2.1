6c728ecad973b5f7a263aa52e0377d939366bda9
Kanat <kanat_007xs@icloud.com>
2026-02-05 21:40:03 +0500

    bool

---
+++ Practice02/boolean/boolean_intro.py
@@ -0,0 +1,23 @@
+#1
+a=int(input())
+b=int(input())
+print(a>b)
+#2
+a=int(input())
+b=int(input())
+if a>b:
+    print("a greater")
+else:
+    print("b greater")
+#3
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+#4
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
