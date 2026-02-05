b603e183d7a1508d547b56eaa3589803eb99aaea
Kanat <kanat_007xs@icloud.com>
2026-02-06 00:28:09 +0500

    break

---
+++ Practice02/loops/while_break.py
@@ -0,0 +1,20 @@
+#1
+a=100
+while(a>0):
+    print(a)
+    if a==25:
+        break
+    i-=1
+#2
+b=100
+while(b>0):
+    print(b)
+    if 100%2==0:
+        break
+    i+i-1
+#3
+while True:
+    c=input()
+    if c.lower()=="exit":
+        break
+    print(c)
