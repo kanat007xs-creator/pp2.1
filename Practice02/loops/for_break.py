a09fde20ccb4e6ef3245786c5560a30505546b73
Kanat <kanat_007xs@icloud.com>
2026-02-06 00:48:38 +0500

    for break

---
+++ Practice02/loops/for_break.py
@@ -0,0 +1,17 @@
+#1
+a="SAlem alem"
+for i in a:
+    print(i)
+    if i=="a":
+        break
+#2
+b=[1,2,4,5,3,456,7,8]
+for i in b:
+    if a%2==1:
+        break
+#3
+c=76
+for i in range(76):
+    print(i)
+    if a%3==0 and a%4==0:
+        break
