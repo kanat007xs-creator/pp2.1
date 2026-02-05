316bdc63e6198dae57969321d4ef4b1b9496a6e9
Kanat <kanat_007xs@icloud.com>
2026-02-05 23:52:47 +0500

    if elif else

---
+++ Practice02/if_else/if_elif_else.py
@@ -0,0 +1,25 @@
+#1
+a1=int(input())
+a2=int(input())
+if (a1+a2)>50:
+    print("great")
+elif (a1+a2)>=30:
+    print("enaugh")
+else:
+    print("bad")
+#2
+fainl=int(input())
+if fainl==40:
+    print("exelent")
+elif fainl>20:
+    print("good")
+else:
+    print("FX")
+#3
+total=a1+a2+fainl
+if total>=95:
+    print("4.0")
+elif total>=70:
+    print("shukir")
+else:
+    print("bad(")
