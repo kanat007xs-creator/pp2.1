892b093db725f0a88973ae00ca47ede7e5543b9c
Kanat <kanat_007xs@icloud.com>
2026-02-05 23:46:16 +0500

    if else

---
+++ Practice02/if_else/if_else.py
@@ -0,0 +1,19 @@
+#1
+a=int(input())
+you=int(input())
+if a>you:
+    print("older sis")
+else:
+    print("litil sis")
+#2
+a=int(input())
+if a>0:
+    print("positive")
+else:
+    print("negative")
+#3
+a=int(input())
+if a%2==0:
+    print("even")
+else:
+    print("odd")
