591af39e8bce8a3436a9eb04cd0fd3d585f79223
Kanat <kanat_007xs@icloud.com>
2026-02-06 00:34:28 +0500

    continue

---
+++ Practice02/loops/while_continue.py
@@ -0,0 +1,21 @@
+#1
+a=154
+while(a>0):
+    print(a)
+    if a%2==0:
+        continue
+    a-=1
+#2
+b=10
+while(a>0):
+    c=input()
+    if c.lower()=="tennis":
+        continue
+    a+=1
+#3
+v=27
+while(v>0):
+    print(v)
+    if v%3!=0:
+        continue
+    v-=1
