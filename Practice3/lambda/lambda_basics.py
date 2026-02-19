5256272dafd52e562041e6b7aeb9157e29f66a56
Kanat <kanat_007xs@icloud.com>
2026-02-20 00:40:00 +0500

    lambda basic

---
+++ Practice3/lambda/lambda_basics.py
@@ -0,0 +1,16 @@
+# 1
+add = lambda a, b: a + b
+print(add(2, 3))
+
+# 2
+square = lambda x: x * x
+print(square(4))
+
+# 3
+is_even = lambda n: n % 2 == 0
+print(is_even(6))
+
+# 4
+greet = lambda name: "Hello " + name
+print(greet("Kanat"))
+
