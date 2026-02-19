851151246429c6bfb41fec6d71c5d5453a5a9016
Kanat <kanat_007xs@icloud.com>
2026-02-20 01:15:55 +0500

    class var

---
+++ Practice3/classes/class_variables.py
@@ -0,0 +1,29 @@
+# 1
+class Person:
+    species = "Human"
+
+p1 = Person()
+p2 = Person()
+print(p1.species)
+print(p2.species)
+
+# 2
+class Car:
+    wheels = 4
+
+c = Car()
+print(c.wheels)
+
+# 3
+class Student:
+    school = "High School"
+
+s = Student()
+print(s.school)
+
+# 4
+class Dog:
+    animal_type = "Mammal"
+
+d = Dog()
+print(d.animal_type)
