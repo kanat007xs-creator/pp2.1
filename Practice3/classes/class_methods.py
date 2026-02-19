f74ea80e40341f311958565276ea79ba5f90b63b
Kanat <kanat_007xs@icloud.com>
2026-02-20 01:15:02 +0500

    class met

---
+++ Practice3/classes/class_methods.py
@@ -0,0 +1,31 @@
+# 1
+class Person:
+    def say_hello(self):
+        print("Hello!")
+
+p = Person()
+p.say_hello()
+
+# 2
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+calc = Calculator()
+print(calc.add(2, 3))
+
+# 3
+class Dog:
+    def bark(self):
+        print("Woof!")
+
+d = Dog()
+d.bark()
+
+# 4
+class Student:
+    def introduce(self, name):
+        print("My name is", name)
+
+s = Student()
+s.introduce("Kanat")
