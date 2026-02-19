ff42ddb3c91cd4aeeff861d846d9eaf847fb913e
Kanat <kanat_007xs@icloud.com>
2026-02-20 00:26:27 +0500

    def argue

---
+++ Practice3/functions/function_arguments.py
@@ -0,0 +1,21 @@
+# 1
+def greet(name):
+    print("Hello,", name)
+
+# 2
+def add(a, b):
+    print(a + b)
+
+# 3
+def multiply(x, y):
+    print(x * y)
+
+# 4
+def introduce(name, age):
+    print("My name is", name, "and I am", age)
+
+
+greet("Kanat")
+add(3, 5)
+multiply(4, 2)
+introduce("Kanat", 17)
