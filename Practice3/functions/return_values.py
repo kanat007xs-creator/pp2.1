50b2618c06ec94426e95d70ecb58fa0d06a667af
Kanat <kanat_007xs@icloud.com>
2026-02-20 00:28:07 +0500

    def return

---
+++ Practice3/functions/return_values.py
@@ -0,0 +1,21 @@
+# 1
+def add(a, b):
+    return a + b
+
+# 2
+def square(x):
+    return x * x
+
+# 3
+def is_even(n):
+    return n % 2 == 0
+
+# 4
+def get_name():
+    return "Kanat"
+
+
+print(add(2, 3))
+print(square(4))
+print(is_even(6))
+print(get_name())
