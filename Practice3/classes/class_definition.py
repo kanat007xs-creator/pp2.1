4e5ce5ac33efc2a9e30eeaab08eca5d2a5d41ce0
Kanat <kanat_007xs@icloud.com>
2026-02-20 01:12:46 +0500

    class def

---
+++ Practice3/classes/class_definition.py
@@ -0,0 +1,30 @@
+# 1
+class Person:
+    pass
+
+p1 = Person()
+print(p1)
+
+# 2
+class Dog:
+    def bark(self):
+        print("Woof!")
+
+d = Dog()
+d.bark()
+
+# 3
+class Car:
+    def drive(self):
+        print("Car is driving")
+
+c = Car()
+c.drive()
+
+# 4
+class Student:
+    def study(self):
+        print("Student is studying")
+
+s = Student()
+s.study()
