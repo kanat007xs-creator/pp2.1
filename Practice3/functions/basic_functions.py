0db26764be33887fd89db4a7080ab40b701fd8cc
Kanat <kanat_007xs@icloud.com>
2026-02-20 00:09:59 +0500

    basic def

---
+++ Practice3/functions/basic_functions.py
@@ -0,0 +1,21 @@
+ # 1
+def say_hello():
+    print("Hello!")
+
+# 2
+def say_name():
+    print("My name is Kanat")
+
+# 3
+def show_number():
+    print(10)
+
+# 4
+def goodbye():
+    print("Goodbye!")
+
+
+say_hello()
+say_name()
+show_number()
+goodbye()
